cc7619479741e22688746b1c692ef68adfad7d7e
Yasir199722 <yasirkhan2211.yk.yk@gmail.com>
2020-06-13 19:25:04 +0500

    JS Assignment deployed

---
+++ App.js
@@ -0,0 +1,646 @@
+Chapter1
+Task1
+// alert("Welcome");
+Task2
+// alert("Error! Please enter a valid password.");
+Task3
+// alert("Welcome to JS Land... \n Happy Coding!");
+Task4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+// alert("Hello... I can run JS from my browser comsole");
+
+Chapter2
+Task1
+// var username = "Yasir";
+Task2
+// var myName = "Yasir Khan";
+Task3
+// var message = alert("Hello World");
+Task4
+// var name = alert("Yasir Khan");
+// var age = alert("22 years old");
+// var skill = alert("Certified Mobile Application Development");
+Task5
+// var pizza = alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+Task6
+// var email = alert("My email address is yasirkhan2211.yk.yk@gmail.com");
+Task7
+// var asmarterwaytolearnJavaScript = alert("I am trying to learn from the book A Smarter Way To Learn JavaScript");
+
+Chapter3
+Task1
+// var age = alert("I am 22 years old");
+Task2
+// var visit = alert("You have visited 14 times this page");
+// var birthyear = "1997";
+// var vistorname = "John";
+// var product = "T-shirt";
+// var quantity = "5";
+
+Chapter4
+Task1
+// var name,age,gender = "yasir,22,male";
+Task2
+// var legal = "name,_,$,number,age";
+// var illegal ="\,<,>,+,;";
+
+Chapter5
+Task1
+// var x = 100 + 50;
+// document.write(x);
+Task2
+// var y = 100 - 50;
+// document.write(y);
+
+// var z = 10 * 2;
+// document.write(z);
+
+// var i = 50 / 2;
+// document.write(i);
+
+// var j = 5 % 2;
+// document.write(j);
+Task3
+// var x = 5;
+// document.write(x);
+// x+6+7;
+// x--;
+// x/3;
+Task4
+// var ticketprice = 600*5;
+// document.write(ticketprice);
+Task5
+// document.write("4" + "x" + "1" +"=" + 4*1 + "<br>")
+// document.write("4" + "x" + "2" +"=" + 4*2 + "<br>")
+// document.write("4" + "x" + "3" +"=" + 4*3 + "<br>")
+// document.write("4" + "x" + "4" +"=" + 4*4 + "<br>")
+// document.write("4" + "x" + "5" +"=" + 4*5 + "<br>")
+// document.write("4" + "x" + "6" +"=" + 4*6 + "<br>")
+// document.write("4" + "x" + "7" +"=" + 4*7 + "<br>")
+// document.write("4" + "x" + "8" +"=" + 4*8 + "<br>")
+// document.write("4" + "x" + "9" +"=" + 4*9 + "<br>")
+// document.write("4" + "x" + "10" +"=" + 4*10 + "<br>")
+
+Task6
+// var celcius = 35;
+// var fahrenheit = 95;
+// document.write("(95-32)"+"*"+"5/9"+"="+"35C"+ "<br>")
+// document.write("(35*9/5)"+"+"+"32"+"="+"95F"+"<br>")
+
+Task7
+// var item1 = 650;
+// var item2 = 100;
+// var quantity1 = 3;
+// var quantity2 = 7;
+// var shipping = 100;
+// document.write("650"+"x"+"3"+"="+ 650*3 + "<br>")
+// document.write("100"+"x"+"7"+"="+ 100*7 + "<br>")
+// document.write("100"+ "<br>")
+// document.write("1950"+"+"+"700"+"+"+"100"+ "="+ ((+1950)+(+700)+(+100)) + "<br>")
+
+Task8
+// var total = 980;
+// var obtained = 804;
+// document.write("804"+"/"+"980"+"*"+"100"+"="+ 804/980*100)
+Task9
+// document.write("104.80"+"x"+"10"+"="+ 104.80*10 + "<br>")
+// document.write("28"+"x"+"25"+"="+ 28*25 + "<br>")
+// document.write("1048"+"+"+"700"+"="+ ((+1048)+(+700)) +"<br>")
+Task10
+// var x = 5;
+// document.write("5"+"+"+"5"+"*"+"10"+"/"+"2"+"="+ ((+5)+(+5))*10/2)
+Task11
+// var currentyear = 2020;
+// var birthyear = 1997;
+// document.write("2020"+"-"+"1997"+"="+ 2020-1997)
+
+Task12
+// var radius = 20;
+// document.write("2"+"*"+"3.142"+"*"+"20"+"="+ 2*3.142*20 +"<br>")
+// document.write("3.142"+"*"+"20"+"*"+"20"+"="+ 3.142*20*20)
+Task13
+// var snack = chocolatechip;
+// var currentage = 15;
+// var  maximumage = 65;
+// var estimatedamount = 3;
+// document.write("65"+"-"+"15"+"*"+"3"+"="+ 65-15*3 +"<br>")
+
+Chapter6-9
+Task1
+// var num = 10;
+// document.write("10"+"+"+"1"+"="+((+10)+(+1))+"<br>");
+// document.write("11"+"+"+"1"+"="+((+11)+(+1))+"<br>");
+// document.write("12"+"-"+"1"+"="+((+12)-(+1))+"<br>");
+// document.write("11"+"-"+"1"+"="+((+11)-(+1))+"<br>");
+task2
+// var a = 2, b = 1;
+// var result = --a - b-- + ++b + b--;
+Task3
+//   prompt("Enter Your Name:");
+//   alert("Welcome");
+Task5
+// prompt("Enter any number:");
+// document.write("5" + "x" + "1" +"=" + 5*1 + "<br>")
+// document.write("5" + "x" + "2" +"=" + 5*2 + "<br>")
+// document.write("5" + "x" + "3" +"=" + 5*3 + "<br>")
+// document.write("5" + "x" + "4" +"=" + 5*4 + "<br>")
+// document.write("5" + "x" + "5" +"=" + 5*5 + "<br>")
+// document.write("5" + "x" + "6" +"=" + 5*6 + "<br>")
+// document.write("5" + "x" + "7" +"=" + 5*7 + "<br>")
+// document.write("5" + "x" + "8" +"=" + 5*8 + "<br>")
+// document.write("5" + "x" + "9" +"=" + 5*9 + "<br>")
+// document.write("5" + "x" + "10" +"=" + 5*10 + "<br>")
+
+Task6
+// prompt("Enter Your Obtained Marks In English:");
+// prompt("Enter Your Obtained Marks  In Maths:");
+// prompt("Enter Your Obtained Marks In Urdu:");
+// document.write("54"+"/"+"100"+"*"+"100"+"="+ 54/100*100+"<br>");
+// document.write("54"+"/"+"100"+"*"+"100"+"="+ 54/100*100+"<br>");
+// document.write("46"+"/"+"100"+"*"+"100"+"="+ 46/100*100+"<br>");
+// document.write("152"+"/"+"300"+"*"+"100"+"="+ 152/300*100+ "<br>");
+
+
+Chapter9-11
+Task1
+// var city =  prompt("Enter City");
+// if(city === "Karachi"){
+//     alert("Welcome To City Of Lights");
+// }
+Task2
+// var gender = prompt("Enter Gender");
+// if (gender === "Male"){
+//     alert("Welcome Sir")
+// }
+// if(gender === "Female"){
+//     alert("Welcome Maam")
+// }
+Task3
+// var color = prompt("Enter Traffic Light Color");
+// if (color === "red"){
+//     alert("Must Stop")
+// }
+// if (color === "yellow"){
+//     alert("Ready To Move")
+// }
+// if(color === "green"){
+//     alert("Move Now")
+// }
+Task4
+//  var fuel= prompt("Enter Fuel");
+// if (fuel === "0.25L"){
+//     alert("Please refill the fuel in your car");
+// }
+Task6
+// var TotalMarks = 300;
+// var Marks = prompt("Enter Your Maths Marks");
+// prompt("Enter Your Chemistry Marks");
+// prompt("Enter Your Physics Marks");
+// if(Marks === 80){
+//     alert("A-one Grade");
+//     alert("Excellent")
+// }
+// if(Marks === 70){
+//     alert("A Grade");
+//     alert("Good");
+// }
+// if(Marks === 60){
+//     alert("B Grade");
+//     alert("You need to improve")
+// }
+// if(Marks === 60){
+//     alert("Fail");
+//     alert("Sorry");
+// }
+// document.write("219"+"/"+"300"+"*"+"100"+"="+ 219/300*100)
+Task7
+// var number = prompt("Guess the Secret Number")
+// if(number === "1"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "2"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "3"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "4"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "5"){
+//     alert("Bingo! Correct answer");
+// }
+// if(number === "6"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "7"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "8"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "9"){
+//     alert("Close enough to the correct answer");
+// }
+// if(number === "10"){
+//     alert("Close enough to the correct answer");
+// }
+
+Task8
+// var number = prompt("Enter Your Number")
+// if(number === "3"){
+//     alert("Number is divisble by 3");
+
+// }
+// if(number === "4"){
+    
+// }
+// if(number === "5"){
+    
+// }
+// if(number === "6"){
+//     alert("Number is divisble by 3");
+    
+// }
+// if(number === "8"){
+    
+// }
+// if(number === "9"){
+//     alert("Number is divisble by 3");
+    
+// }
+// if(number === "12"){
+//     alert("Number is divisble by 3");
+    
+// }
+Task9
+// var number = prompt("Enter Your Number")
+// if (number === "1"){
+//     alert("Odd Number")
+// }
+// if (number === "2"){
+//     alert("Even Number")
+// }
+// if (number === "3"){
+//     alert("Odd Number")
+// }
+// if (number === "4"){
+//     alert("Even Number")
+// }
+// if (number === "5"){
+//     alert("Odd Number")
+// }
+// if (number === "6"){
+//     alert("Even Number")
+// }
+// if (number === "7"){
+//     alert("Odd Number")
+// }
+// if (number === "8"){
+//     alert("Even Number")
+// }
+// if (number === "9"){
+//     alert("Odd Number")
+// }
+// if (number === "10"){
+//     alert("Even Number")
+// }
+Task10
+// var temperature = prompt("Enter Temperature")
+// if (temperature === "40"){
+//     alert("Its too hot outside")
+// }
+// if (temperature === "30"){
+//     alert("The weather today is normal")
+// }
+// if (temperature === "20"){
+//     alert("Today's weather is so cool")
+// }
+// if (temperature === "10"){
+//     alert("OMG! Today's weather is so cool")
+// }
+Task11
+// var val1 = prompt("Enter Your First value")
+// var sign = prompt("Enter Your Operator")
+// var val2 = prompt("Enter Your Second value")
+
+// console.log(val1+sign+val2)
+// if(sign === "+"){
+//     alert((+val1)+(+val2))
+// }
+// else if(sign === "-"){
+//     alert(val1-val2)
+// }
+// else if(sign === "*"){
+//     alert(val1*val2)
+// }
+// else if(sign === "/"){
+//     alert(val1/val2)
+// }
+// else if(sign === "%"){
+//     alert(val1/val2*100 + "%")
+// }
+
+Chapter12-13
+Task1
+// var alphabets = prompt("Enter Your Alphabet or Number")
+// if(alphabets === "A"){
+//     alert("It is an uppercase alphabet");
+// }
+// if(alphabets === "a"){
+//     alert("It is an lowercase alphabet");
+// }
+// if(alphabets === "B"){
+//     alert("It is an uppercase alphabet");
+// }
+// if(alphabets === "b"){
+//     alert("It is an lowercase alphabet");
+// }
+// if(alphabets === "C"){
+//     alert("It is an uppercase alphabet");
+// }
+// if(alphabets === "c"){
+//     alert("It is an lowercase alphabet");
+// }
+// if(alphabets === "D"){
+//     alert("It is an uppercase alphabet");
+// }
+// if(alphabets === "d"){
+//     alert("It is an lowercase alphabet");
+// }
+// if(alphabets === "1"){
+//     alert("It is a Number");
+// }
+// if(alphabets === "2"){
+//     alert("It is a Number");
+// }
+// if(alphabets === "3"){
+//     alert("It is a Number");
+// }
+// if(alphabets === "4"){
+//     alert("It is a Number");
+// }
+Task2
+// var integer1 = prompt("Enter Your First Integer")
+// var integer2 = prompt("Enter Your Second Integer")
+// if(integer1 > integer2){
+//     alert("Integer 1 is larger number")
+// }
+// if(integer1 < integer2){
+//     alert("Integer 2 is larger number")
+// }
+// if(integer1 === integer2){
+//     alert("Both Integers are equal")
+// }
+Task3
+// var numbers = prompt("Enter Your Number")
+// if (numbers === "+"){
+//     alert("It is a Positive Number")
+// }
+// if (numbers === "-"){
+//     alert("It is a negative number")
+// }
+// if (numbers === "0"){
+//     alert("It is zero")
+// }
+Task4
+// var alphabet = prompt("Enter Your Alphabet")
+// if(alphabet === "a"){
+//     alert("True")
+// }
+// if(alphabet === "e"){
+//     alert("True")
+// }
+// if(alphabet === "i"){
+//     alert("True")
+// }
+// if(alphabet === "o"){
+//     alert("True")
+// }
+// if(alphabet === "u"){
+//     alert("True")
+// }
+// if(alphabet === "b"){
+//     alert("False")
+// }
+// if(alphabet === "c"){
+//     alert("False")
+// }
+// if(alphabet === "s"){
+//     alert("False")
+// }
+// if(alphabet === "r"){
+//     alert("False")
+// }
+// if(alphabet === "y"){
+//     alert("False")
+// }
+Task5
+// var Password1 = prompt("Enter Your Password")
+// var password2 = prompt("Confirm Your Password")
+// if(Password1 === "null"){
+//     alert("Enter Your Password")
+// }
+// if(Password1 === "pass"){
+//     alert("Correct")
+// }
+// if(Password1 !== "pass"){
+//     alert("wrong answer")
+// }
+Task6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+Task7
+// var time = prompt("Enter Time");
+// if(time >= 0000 && time < 1200){
+//     alert("Good Morning!");
+// }
+// else if(time >= 1200 && time < 1700){
+//     alert("Good Afternoon!");
+// }
+// else if(time >=1700 && time < 2100){
+//     alert("Good Evening!");
+// }
+// else if(time >= 2100 && time <= 2359){
+//     alert("Good Night!")
+// }
+
+Chapter14-16
+Task1
+// var student = []
+// var students = []
+// var string = []
+// var number = []
+// var bool = []
+// var mixed = []
+// var qualification = [SSC, HSC, BCS, BS ,BCOM, MS, MPHIL, PHD ]
+
+Task2
+// var arr = ["Michael", "John", "Tony"];
+// var number = ["320", "230", "480"];
+// document.write("320"+"/"+"500"+"*"+"100"+"="+ 320/500*100+"%"+"<br>");
+// document.write("230"+"/"+"500"+"*"+"100"+"="+ 230/500*100+"%"+"<br>");
+// document.write("480"+"/"+"500"+"*"+"100"+"="+ 480/500*100+"%"+"<br>");
+
+Task3
+// var color = ["Purple", "Blue", "Red", "Yellow"];
+// alert( color[0] );
+// alert( color[1] );
+// alert( color[2] );
+// alert( color[3] );
+// color.push("Green");
+// alert(color);
+// color.push("Pink","Orange");
+// alert(color);
+// color.pop(0);
+// alert(color);
+// color.pop(6);
+// alert(color);
+// color[2] = "Brown";
+// alert(color);
+// color.pop(5);
+// alert(color);
+
+Task4
+// var students = [320, 230, 480, 120]
+// alert(students.sort());
+
+Task5
+// var cities = ["Lahore", "Islamabad", "Faisalabad","Karachi","Quetta"]
+// var selectedcities = ["Islamabad", "Quetta"]
+// for(var i = 0; i<cities.length; i++){
+//     if(cities[i] === "Islamabad"){
+//         alert(cities[i]+ " is a selected city")
+//     }
+//     else if(cities[i] === "Quetta"){
+//         alert(cities[i]+ " is a selected city")
+//     }
+// }
+
+Task6
+// var arr = ["This", "is", "my", "cat"];
+// alert(arr.join(' '));
+
+Task7
+// var devices = ["Keybroad", "Mouse", "Printer", "Monitor"];
+// alert(devices);
+// devices.shift(0);
+// alert(devices);
+// devices.shift(1);
+// alert(devices);
+// devices.shift(2);
+// alert(devices);
+// devices.shift(3);
+// alert(devices);
+
+Task8
+// var devices = ["Keybroad", "Mouse", "Printer", "Monitor"];
+// alert(devices);
+// devices.pop(0);
+// alert(devices);
+// devices.pop(1);
+// alert(devices);
+// devices.pop(2);
+// alert(devices);
+// devices.pop(3);
+// alert(devices);
+
+Chapter17-20
+Task1
+// var arr = [[1,2],[3,4],[5,6]];
+
+Task2
+// var arr1 = ["0", 1, 2, 3]; 
+// var arr2 = ["1", 0, 1, 2]; 
+// var arr3 = ["2", 1, 0, 2];   
+// var matrix = [arr1, arr2, arr3,];  
+// for(var i = 0; i < matrix.length; i++) { 
+//     document.write(matrix[i] + "<br>"); 
+// }
+
+Task3
+// var numbers;
+// for (var numbers = 1; numbers <= 10; numbers++) {
+//     document.write(numbers);
+//    }
+
+Task4
+// var num1 = prompt("Enter a number to show his multiplication table");
+// var num2 = prompt("Enter length of multiplication table ")
+// for (i=1;i<num1 + 1;i++) {
+// 	for (j=1;j<num2 + 1;j++) {
+// 	if (j == 10) {
+// 	document.write(i+"*" +j +"=" +(i*j) + "<br /><br />");
+// 	} else {
+// 	document.write(i+"*" +j +"=" +(i*j) + "  |  ");
+// 	}
+// 	}
+// } 
+
+Task5
+// var fruits = ["Apple", "Banana", "Mango", "Orange", "Strawberry"];
+// var len = fruits.length;
+// for (var i = 0; i < fruits.length; i++) {
+// 	document.write(fruits[i] + "<br>");
+// }
+
+Task7
+// var arr =  ["Cake", "Apple Pie", "Cookie", "Chips", "Paties"];
+// input = prompt("Welcome To ABC Bakery. What do you want to order sir/ma'am?");
+// if(input === "Cake"){
+//     alert("Cake is available at index0 in our bakery")
+// }
+// if(input === "Apple Pie"){
+//     alert("Apple Pie is available at index1 in our bakery")
+// }
+// if(input === "Cookie"){
+//     alert("Cookie is available at index2 in our bakery")
+// }
+// if(input === "Chips"){
+//     alert("Chips is available at index3 in our bakery")
+// }
+// if(input === "Paties"){
+//     alert("Paties is available at index4 in our bakery")
+// }
+// if(input === "Pastry"){
+// 	alert("Pastry is not available in our bakery!")
+// }
+
+Task8
+// var A = ["24", "53", "78", "91", "12"];
+// var largest= 0;
+
+// for (i=0; i<=largest;i++){
+//     if (A[i]>largest) {
+//         var largest=A[i];
+//     }
+// }
+// document.write(A+"<br>")
+// document.write(largest);
+
+Task9
+// var A = [ "12", "56", "89", "65", "61", "36", "45", "23" ];
+// var smallest = A[0];
+
+// for(var i=1; i<A.length; i++){
+//     if(A[i] < smallest){
+//         smallest = A[i];   
+//     }
+// }
+// document.write(A+ "<br>")
+// document.write(smallest);
+
+Task10
+// var A = ["5","10","15","20","25","30","35","40","45","50","55","60","65","70","80","85","90","95","100"];
+// document.write(A+"<br>");
+
+
